1ca8a53f5689e612b8e536aaa9f432566ccdf53d
Tauane Lima <Tauanesantos10@hotmail.com>
2021-04-23 17:13:59 -0300

    Exe1 feito

--- src/Exe1.js
+++ src/Exe1.js
@@ -20,6 +20,25 @@
  * @returns {Number} resultado da operação
  */
 function operacoesBasicas(operacao, numero1, numero2) {
+    if(operacao == "A") {
+       return numero1 + numero2
+    } else if (operacao == "S") {
+       return numero1 - numero2
+    } else if (operacao == "M") {
+       return numero1 * numero2 
+    } else if (operacao == "D") {
+        if (numero1 != 0 && numero2 != 0) {
+            return numero1 / numero2 
+        } else {
+            return NaN
+        } 
+    } else if (operacao == "P"){
+       return numero1 ** numero2
+    } else if (typeof (numero1) != "number" || typeof (numero2) != "number") {
+        return NaN
+    } else if (operacao != "A" || "S" || "M" || "D" || "P"){
+        return undefined
+    }
 }
 
 /**
@@ -34,5 +53,18 @@ function operacoesBasicas(operacao, numero1, numero2) {
  * @param {*} elemento2
  * @returns {string} mensagem com o resultado da comparação 
  */
-function comparadorBasico(elemento1, elemento2) {
+ function comparadorBasico(elemento1, elemento2) {
+    if (typeof (elemento1) == typeof (elemento2) && elemento1 == elemento2) {
+
+        return 'Elemento ' + elemento1 + " (" + typeof (elemento1) + ") é estritamente igual ao elemento " + elemento2 + " (" + typeof (elemento2) + ")"
+
+    } else if (elemento1 != elemento2) {
+
+        return 'Elemento ' + elemento1 + " (" + typeof (elemento1) + ") é diferente do elemento " + elemento2 + " (" + typeof (elemento2) + ")"
+
+    } else if (elemento1 == elemento2 && typeof (elemento1) != typeof (elemento2)) {
+
+        return 'Elemento ' + elemento1 + " (" + typeof (elemento1) + ") é equivalente ao elemento " + elemento2 + " (" + typeof (elemento2) + ")"
+
+    }
 }
